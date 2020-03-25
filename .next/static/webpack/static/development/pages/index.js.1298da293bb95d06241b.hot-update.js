e7fc8e5aeae68b37722293076229910df3dc2425
Stuard Gerardo Carrillo Gonzalez <sgcg5@hotmail.com>
2020-03-24 18:25:18 -0600

    hello world

---
+++ .next/static/webpack/static/development/pages/index.js.1298da293bb95d06241b.hot-update.js
@@ -0,0 +1,51 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/index.tsx":
+/*!*************************!*\
+  !*** ./pages/index.tsx ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var styled_jsx_style__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! styled-jsx/style */ "./node_modules/styled-jsx/style.js");
+/* harmony import */ var styled_jsx_style__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(styled_jsx_style__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+var _this = undefined,
+    _jsxFileName = "/home/surface/projects/platzi/next-js-platzi/pages/index.tsx";
+
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement;
+
+
+var Home = function Home() {
+  return __jsx("div", {
+    className: "jsx-1549376124",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4,
+      columnNumber: 3
+    }
+  }, __jsx("h1", {
+    className: "jsx-1549376124",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5,
+      columnNumber: 5
+    }
+  }, "Hello world!"), __jsx(styled_jsx_style__WEBPACK_IMPORTED_MODULE_0___default.a, {
+    id: "1549376124",
+    __self: _this
+  }, "h1.jsx-1549376124:{color:red;}\n/*# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi9ob21lL3N1cmZhY2UvcHJvamVjdHMvcGxhdHppL25leHQtanMtcGxhdHppL3BhZ2VzL2luZGV4LnRzeCJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFNZ0IsQUFJTSxVQUFDIiwiZmlsZSI6Ii9ob21lL3N1cmZhY2UvcHJvamVjdHMvcGxhdHppL25leHQtanMtcGxhdHppL3BhZ2VzL2luZGV4LnRzeCIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBSZWFjdCwgeyBGQyB9IGZyb20gJ3JlYWN0J1xuXG5jb25zdCBIb21lOiBGQyA9ICgpID0+IChcbiAgPGRpdj5cbiAgICA8aDE+SGVsbG8gd29ybGQhPC9oMT5cblxuICAgIDxzdHlsZSBqc3g+e2BcbiAgICAgIGgxOiB7XG4gICAgICAgIGNvbG9yOiByZWRcbiAgICAgIH1cbiAgICBgfTwvc3R5bGU+XG4gIDwvZGl2PlxuKTtcblxuZXhwb3J0IGRlZmF1bHQgSG9tZTsiXX0= */\n/*@ sourceURL=/home/surface/projects/platzi/next-js-platzi/pages/index.tsx */"));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Home);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.1298da293bb95d06241b.hot-update.js.map
---
+++ .next/static/webpack/static/development/pages/index.js.45a47b43234480d325b9.hot-update.js
@@ -0,0 +1,54 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/index.tsx":
+/*!*************************!*\
+  !*** ./pages/index.tsx ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var styled_jsx_style__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! styled-jsx/style */ "./node_modules/styled-jsx/style.js");
+/* harmony import */ var styled_jsx_style__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(styled_jsx_style__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+var _this = undefined,
+    _jsxFileName = "/home/surface/projects/platzi/next-js-platzi/pages/index.tsx";
+
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement;
+
+
+var Home = function Home() {
+  return __jsx("div", {
+    className: "jsx-262491599",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4,
+      columnNumber: 3
+    }
+  }, __jsx("h1", {
+    className: "jsx-262491599",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5,
+      columnNumber: 5
+    }
+  }, "Hello world!"), __jsx(styled_jsx_style__WEBPACK_IMPORTED_MODULE_0___default.a, {
+    id: "732224013",
+    __self: _this
+  }, "h1.jsx-262491599:{color:red;}\n/*# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi9ob21lL3N1cmZhY2UvcHJvamVjdHMvcGxhdHppL25leHQtanMtcGxhdHppL3BhZ2VzL2luZGV4LnRzeCJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFNZ0IsQUFHbUIsVUFDWiIsImZpbGUiOiIvaG9tZS9zdXJmYWNlL3Byb2plY3RzL3BsYXR6aS9uZXh0LWpzLXBsYXR6aS9wYWdlcy9pbmRleC50c3giLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgUmVhY3QsIHsgRkMgfSBmcm9tIFwicmVhY3RcIjtcblxuY29uc3QgSG9tZTogRkMgPSAoKSA9PiAoXG4gIDxkaXY+XG4gICAgPGgxPkhlbGxvIHdvcmxkITwvaDE+XG5cbiAgICA8c3R5bGUganN4PntgXG4gICAgICBoMToge1xuICAgICAgICBjb2xvcjogcmVkO1xuICAgICAgfVxuICAgIGB9PC9zdHlsZT5cbiAgICA8c3R5bGUganN4IGdsb2JhbD57YFxuICAgICAgYm9keToge1xuICAgICAgICBiYWNrZ3JvdW5kOiBibHVlO1xuICAgICAgfVxuICAgIGB9PC9zdHlsZT5cbiAgPC9kaXY+XG4pO1xuXG5leHBvcnQgZGVmYXVsdCBIb21lO1xuIl19 */\n/*@ sourceURL=/home/surface/projects/platzi/next-js-platzi/pages/index.tsx */"), __jsx(styled_jsx_style__WEBPACK_IMPORTED_MODULE_0___default.a, {
+    id: "1930456971",
+    __self: _this
+  }, "body:{background:blue;}\n/*# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi9ob21lL3N1cmZhY2UvcHJvamVjdHMvcGxhdHppL25leHQtanMtcGxhdHppL3BhZ2VzL2luZGV4LnRzeCJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFXdUIsQUFHeUIsZ0JBQ2xCIiwiZmlsZSI6Ii9ob21lL3N1cmZhY2UvcHJvamVjdHMvcGxhdHppL25leHQtanMtcGxhdHppL3BhZ2VzL2luZGV4LnRzeCIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBSZWFjdCwgeyBGQyB9IGZyb20gXCJyZWFjdFwiO1xuXG5jb25zdCBIb21lOiBGQyA9ICgpID0+IChcbiAgPGRpdj5cbiAgICA8aDE+SGVsbG8gd29ybGQhPC9oMT5cblxuICAgIDxzdHlsZSBqc3g+e2BcbiAgICAgIGgxOiB7XG4gICAgICAgIGNvbG9yOiByZWQ7XG4gICAgICB9XG4gICAgYH08L3N0eWxlPlxuICAgIDxzdHlsZSBqc3ggZ2xvYmFsPntgXG4gICAgICBib2R5OiB7XG4gICAgICAgIGJhY2tncm91bmQ6IGJsdWU7XG4gICAgICB9XG4gICAgYH08L3N0eWxlPlxuICA8L2Rpdj5cbik7XG5cbmV4cG9ydCBkZWZhdWx0IEhvbWU7XG4iXX0= */\n/*@ sourceURL=/home/surface/projects/platzi/next-js-platzi/pages/index.tsx */"));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Home);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.45a47b43234480d325b9.hot-update.js.map
